2ef4b69e807c6249162fa4c119f5612073d59349
seth <furman.seth@gmail.com>
2017-12-18 16:58:06 -0500

    update unpack.sh to use lsconfigs.sh

--- unpack.sh
+++ unpack.sh
@@ -1,23 +1,16 @@
 #!/usr/bin/env bash
 
-if [ ! -d configs/ ]; then
-    echo "Directory \"configs/\" missing"
-    exit 1
-fi
-
-# print options
-echo "configurations"
-echo "--------------"
-ls configs | sed 's/.tar.gz//g' | nl
-echo
-
 # prompt for configuration
-read -p 'configuration name:' archive
-archive="$(basename $archive)".tar.gz
+archive="$1"
+if [ -z "$archive" ]; then
+    ./lsconfigs.sh
+    read -p 'configuration name:' archive
+fi
+archive="$(basename $archive)"
 
-if [ ! -f configs/$archive ]; then
-    echo "Archive \"$archive\" not found!"
+if [ ! -f configs/$archive.tar.gz ]; then
+    echo "Archive \"$archive.tar.gz\" not found!"
     exit 1
 fi
 
-tar -xf configs/$archive
+tar -xf configs/$archive.tar.gz
